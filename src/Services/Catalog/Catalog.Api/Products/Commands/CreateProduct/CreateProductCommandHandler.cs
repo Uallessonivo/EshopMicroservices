26a3f1abd10b893a289ac13da8c1f7fd9f01ccf6
Uallessonivo <uallessons@gmail.com>
2024-03-15 16:17:56 -0300

    wip(Products): validation pipeline and behaviors & validating CreateProduct command

--- src/Services/Catalog/Catalog.Api/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -3,12 +3,25 @@ using Catalog.Api.Products.Commands.CreateProduct.Result;
 
 namespace Catalog.Api.Products.Commands.CreateProduct;
 
-internal class CreateProductCommandHandler(IDocumentSession session, Logger<CreateProductCommandHandler> logger)
+internal class CreateProductCommandHandler(
+    IDocumentSession session,
+    Logger<CreateProductCommandHandler> logger,
+    IValidator<CreateProductCommand> validator)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+
+        if (errors.Count != 0)
+        {
+            logger.LogWarning("CreateProductCommandHandler.Handle validation failed with errors: {@Errors}", errors);
+            throw new ValidationException(errors.FirstOrDefault());
+        }
+
         var product = new Product
         {
             Name = command.Name,
@@ -16,7 +29,7 @@ internal class CreateProductCommandHandler(IDocumentSession session, Logger<Crea
             Description = command.Description,
             Price = command.Price
         };
-        
+
         session.Store(product);
         await session.SaveChangesAsync(cancellationToken);
 
--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,15 +1,22 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
 
