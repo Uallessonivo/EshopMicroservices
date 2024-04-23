22b95c93b50881f0a979b6d2925fe3c7238a2ce6
Uallessonivo <uallessons@gmail.com>
2024-04-23 15:25:34 -0300

    feat: EntityFrameworkCore.Design package, initializing data and API services within the program.

--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,17 +1,24 @@
 using Ordering.Api;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
     .AddApplicationServices()
-    .AddInfrastructure(builder.Configuration)
+    .AddInfrastructureServices(builder.Configuration)
     .AddApiServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
