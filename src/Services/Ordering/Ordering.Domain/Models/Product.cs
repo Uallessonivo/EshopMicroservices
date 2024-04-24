272cc56c1a77e2af49c71b4880251caba55cbcc0
Uallessonivo <uallessons@gmail.com>
2024-04-24 10:58:24 -0300

    feat: Introduce a new static method Create in Product.cs

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -4,5 +4,19 @@
     {
         public string Name { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
+
+        public static Product Create(ProductId productId, string name, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+
+            var product = new Product
+            {
+                Id = productId,
+                Name = name,
+                Price = price
+            };
+
+            return product;
+        }
     }
 }
