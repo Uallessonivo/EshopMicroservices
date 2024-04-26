a5df0bb733d3455188fae27071fb788509e589d1
Uallessonivo <uallessons@gmail.com>
2024-04-26 10:50:15 -0300

    feat: Add DeleteOrder, this is responsible for handling the deletion of an order and validating it (implementation pending).

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
