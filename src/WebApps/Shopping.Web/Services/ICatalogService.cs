50a291890b19a5d3a18cb49e53ca6e0336ab0431
Uallessonivo <uallessons@gmail.com>
2024-06-17 13:54:20 -0300

    feat: Add Shopping.Web Catalog Service

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {
+        Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+        Task<GetProductsByIdResponse> GetProducts(Guid id);
+        Task<GetProductsByCategoryResponse> GetProductsByCategoryResponse(string category);
+    }
+}
