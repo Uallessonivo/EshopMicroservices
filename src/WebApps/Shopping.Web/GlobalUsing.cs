af4858df6773eec803a7b52fc6bcbd0d44e31915
Uallessonivo <uallessons@gmail.com>
2024-06-19 10:05:05 -0300

    feat: Impl IOrderingService with Refit Http and updating Global usings

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,3 +1,5 @@
-﻿global using Refit;
+﻿global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Refit;
 global using Shopping.Web.Models.Catalog;
 global using Shopping.Web.Services;
